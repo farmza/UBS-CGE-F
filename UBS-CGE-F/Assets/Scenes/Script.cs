337738ac9a72bab12b5b87dd30a397cc84048ec9
pisitsan <109785164+farmza@users.noreply.github.com>
2022-11-01 15:02:32 +0700

    MOVE

---
+++ UBS-CGE-F/Assets/Scenes/Script.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+        }
+    }
+}
